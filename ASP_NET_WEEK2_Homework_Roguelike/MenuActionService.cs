f06bc217cc146d43f6fe46160ccf9ce2df8a0965
hubert kieslich <hubertargo@interia.pl>
2024-07-30 22:32:33 +0200

    Dodaj pliki projektów.

---
+++ ASP_NET_WEEK2_Homework_Roguelike/MenuActionService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_NET_WEEK2_Homework_Roguelike
+{
+    internal class MenuActionService
+    {
+        private List<MenuAction> menuActions;
+
+        public void AddNewAction(int id, string name, string menuName)
+        {
+            MenuAction menuAction = new MenuAction() { Id = id, Name = name, MenuName = menuName };
+            menuActions.Add(menuAction);
+        }
+
+        public List<MenuAction> GetMenuActionsByMenuName(string menuName)
+        {
+            List<MenuAction> result = new List<MenuAction>();
+            foreach(var menuAction in menuActions)
+            {
+                if (menuAction.MenuName==menuName)
+                {
+                    result.Add(menuAction);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ASP_NET_WEEK2_Homework_Roguelike/Program.cs
@@ -0,0 +1,59 @@
+﻿using ASP_NET_WEEK2_Homework_Roguelike;
+using System.Runtime.CompilerServices;
+using static System.Console;
+// Przywitanie użytkownika
+// Pojawienie się main menu
+////a. Nowa gra
+////b. Kontynuuj
+////c. Opis gry
+////d. Wyjdź z gry
+//////a Stworzenie nowej postaci z domyślnymi statystykami
+//////a1. Ruch A/W/S/D
+//////a2. E - sprawdzenie ekwipunku
+//////a3. Q - zapis i wyjście
+//////b Wczytanie statusu poprzedniej postaci
+//////b1. Ruch A/W/S/D
+//////b2. E - sprawdzenie ekwipunku
+//////b3. Q - zapis i wyjście
+/////// a1/b1 Losowanie zdarzeń
+/////// 1) znalezienie przedmiotu (losowanie z puli przedmiotów)
+/////// 2) wydarzenie (losowanie z puli wydarzeń)
+/////// 3) nic się nie dzieje
+/////// 4) spotkanie postaci (losowanie z puli postaci)
+
+MenuActionService actionService = new MenuActionService();t
+actionService = Initialize(actionService);
+
+WriteLine("Welcome to Roguelike game");
+WriteLine("What would like to do?");
+
+var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+for (int i = 0; i < mainMenu.Count; i++)
+{
+    WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+}
+
+var operation = ReadKey();
+switch(operation.KeyChar)
+{
+    case '1':
+        break;
+    case '2':
+        break;
+    case '3':
+        break;
+    case '4':
+        break;
+    default:
+        WriteLine("Wrong input");
+        break;
+}
+
+  static MenuActionService Initialize(MenuActionService actionService)
+{
+    actionService.AddNewAction(1, "New game", "Main");
+    actionService.AddNewAction(2, "Continue", "Main");
+    actionService.AddNewAction(3, "Game description", "Main");
+    actionService.AddNewAction(4, "Quit game", "Main");
+    return actionService;
+}
